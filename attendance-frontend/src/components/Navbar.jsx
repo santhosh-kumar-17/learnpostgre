59a737877c1519be7e74867c97c4617e7e6517c2
santhosh-kumar-17 <santhoshpunk18@gmail.com>
2024-01-03 11:41:27 +0530

    till navbar

---
+++ attendance-frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import Officewebsite from "./Officewebsite";
+import Register from "./Register";
+import Signup from "./Signup";
+
+function Navbar() {
+    return (
+        <h2></h2>
+    )
+}
+
+export default Navbar;
---
+++ attendance-frontend/src/components/Officewebsite.jsx
@@ -0,0 +1,6 @@
+function Officewebsite(){
+    return(
+            <h1>Officewebsite</h1>
+    )
+}
+export default Officewebsite;
